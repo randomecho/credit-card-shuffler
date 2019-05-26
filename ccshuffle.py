3714403f2529871f4aa2a84643ab12a5fb91717e
Soon Van <cog@randomecho.com>
2019-05-26 19:48:50 -0400

    Wrap reading input file behind try block in case
    In case it's not there obviously and need to show the user
    where it was picking up the expected file location.

--- ccshuffle.py
+++ ccshuffle.py
@@ -12,23 +12,27 @@ parser.add_argument("-o", "--output", dest="output_file",
     help="Output location of reformatted CSV (default: %(default)s)")
 args = parser.parse_args()
 
-with open(args.input_file) as csv_file:
-    csv_reader = csv.reader(csv_file)
-    has_header = csv.Sniffer().has_header(csv_file.read(1024))
-    csv_file.seek(0)
-    expenses = []
+expenses = []
 
-    if has_header:
-        next(csv_reader)
+try:
+    with open(args.input_file) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        has_header = csv.Sniffer().has_header(csv_file.read(1024))
+        csv_file.seek(0)
 
-    for row in csv_reader:
-        if float(row[3]) > 0:
-            expenses.append({
-                "transaction_date": datetime.strptime(row[0], '%m/%d/%Y').strftime('%Y-%m-%d'),
-                "description": row[2],
-                "amount": row[3],
-                "category": row[4],
-                })
+        if has_header:
+            next(csv_reader)
+
+        for row in csv_reader:
+            if float(row[3]) > 0:
+                expenses.append({
+                    "transaction_date": datetime.strptime(row[0], '%m/%d/%Y').strftime('%Y-%m-%d'),
+                    "description": row[2],
+                    "amount": row[3],
+                    "category": row[4],
+                    })
+except FileNotFoundError:
+    print("Cannot open transactions file: " + args.input_file)
 
 if expenses:
     expenses.sort(key=itemgetter("transaction_date"))
