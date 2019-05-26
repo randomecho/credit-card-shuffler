bd54f783348f1e1e3f15495f84648efa80239a83
Soon Van <cog@randomecho.com>
2019-05-26 14:09:28 -0400

    Use variables to stash input, output file location

--- ccshuffle.py
+++ ccshuffle.py
@@ -1,6 +1,9 @@
 import csv
 
-with open('/tmp/activity.csv') as csv_file:
+input_file = '/tmp/activity.csv'
+output_file = '/tmp/activity-processed.csv'
+
+with open(input_file) as csv_file:
     csv_reader = csv.reader(csv_file)
     has_header = csv.Sniffer().has_header(csv_file.read(1024))
     csv_file.seek(0)
@@ -15,7 +18,7 @@ with open('/tmp/activity.csv') as csv_file:
                 "transaction_date": row[0],
                 "description": row[2],
                 "amount": row[3],
-                "category": row[4]
+                "category": row[4],
                 })
 
 print(expenses)
